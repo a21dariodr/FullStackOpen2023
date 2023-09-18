a149d8c4eb24f2bce3e51f5c138d3ad64a5339c2
a21dariodr <a21dariodr@iessanclemente.net>
2023-09-19 01:31:05 +0200

    Exercise 1.1 modificated

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,9 +6,17 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
+    <>
+      <p>
+        {props.contents[0].part} {props.contents[0].exercises}
+      </p>
+      <p>
+        {props.contents[1].part} {props.contents[1].exercises}
+      </p>
+      <p>
+        {props.contents[2].part} {props.contents[2].exercises}
+      </p>
+    </>
   )
 }
 
@@ -32,9 +40,11 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content contents={[
+        {part: part1, exercises: exercises1},
+        {part: part2, exercises: exercises2},
+        {part: part3, exercises: exercises3}
+      ]} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
