1d9cb86b63bde14b7c6d014baef8ec8e5754e7c6
a21dariodr <a21dariodr@iessanclemente.net>
2023-09-22 20:49:41 +0200

    Exercise 1.8 finished

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,10 +1,44 @@
 import { useState } from 'react'
 
+// Destructuring the props object
+const Statistics = ({ feedback }) => {
+  // Destructuring the feedback array
+  const [good, neutral, bad] = feedback
+
+  const all = good + neutral + bad
+  // Ternary operator to avoid showing NaN when all is cero
+  const average = (all === 0) ? 0 : ((good*1 + neutral*0 + bad*-1) / all)
+  const positive = (all === 0) ? 0 : ((good / all) * 100)
+
+  console.log('all', all)
+  console.log('average', average)
+  console.log('positive', positive)
+
+  return (
+    <>
+      <h1>statistics</h1>
+      <div>
+        good {good}<br/>
+        neutral {neutral}<br/>
+        bad {bad}<br/>
+        all {all}<br/>
+        average {average}<br/>
+        positive {positive} %<br/>
+      </div>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  console.log('Rendering...')
+  console.log('good', good)
+  console.log('neutral', neutral)
+  console.log('bad', bad)
 
   return (
     <div>
@@ -12,15 +46,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>statistics</h1>
-      <div>
-        good {good}<br/>
-        neutral {neutral}<br/>
-        bad {bad}<br/>
-        all {good + neutral + bad}<br/>
-        average {(good*1 + neutral*0 + bad*-1) / (good + neutral + bad)}<br/>
-        positive {(good / (good + neutral + bad)) * 100} %<br/>
-      </div>
+      <Statistics feedback={[good, neutral, bad]} />
     </div>
   )
 }
