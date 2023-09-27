67a996e12c8fade5513ca6f0b42c4f3515ac6078
a21dariodr <a21dariodr@iessanclemente.net>
2023-09-27 02:12:56 +0200

    Exercise 2.10 finished

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -43,23 +47,21 @@ const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
-      filter shown with <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+      <Filter filter={filter} handleNewFilter={(e) => setFilter(e.target.value)} />
       <h2>Add a new contact</h2>
-      <form onSubmit={handleNewPerson}>
-        <div>
-          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
-        </div>
-        <div>
-          number: <input type='tel' value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm 
+        states={{
+          newName: newName,
+          newNumber: newNumber
+        }}
+        handlers={{
+          handleNewName: (e) => setNewName(e.target.value),
+          handleNewNumber: (e) => setNewNumber(e.target.value),
+          handleSubmit: handleNewPerson
+        }}
+      />
       <h2>Numbers</h2>
-      {filteredPersons.map( person => 
-        <p key={person.id}>{person.name} {person.number}</p>
-      )}
+      <Persons persons={filteredPersons} />
     </div>
   )
 }
