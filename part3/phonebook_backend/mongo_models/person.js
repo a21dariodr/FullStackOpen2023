abba3e77fbbe6ee36acdfd14bed74422f3cc134d
a21dariodr <a21dariodr@iessanclemente.net>
2024-03-02 13:38:05 +0100

    Finished exercise 3.20

--- part3/phonebook_backend/mongo_models/person.js
+++ part3/phonebook_backend/mongo_models/person.js
@@ -15,7 +15,17 @@ const personSchema = new Schema({
       minLength: 3,
       required: true
     },
-    number: String
+    number: {
+      type: String,
+      minLength: 8,
+      validate: {
+        validator: function(number) {
+          return /^\d{2,3}-\d+$/.test(number)
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      },
+      required: true
+    }
 })
 
 personSchema.set('toJSON', {
