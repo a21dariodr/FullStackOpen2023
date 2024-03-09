9adc71266826f56476da2a2f0045515cdb4a555a
a21dariodr <a21dariodr@iessanclemente.net>
2024-03-09 11:14:27 +0100

    Finished exercise 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,31 @@
+const { MONGODB_URI } = require('./utils/config')
+const express = require('express')
+const app = express()
+const blogRouter = require('./controllers/blogs')
+const cors = require('cors')
+const logger = require('./utils/logger')
+const { requestLogger, unknownEndpoint, errorHandler } = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', MONGODB_URI)
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
+
+
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+module.exports = app
