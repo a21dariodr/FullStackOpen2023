9febe2e65089b8bd8d81a99716d461e52742ce6e
a21dariodr <a21dariodr@iessanclemente.net>
2024-04-13 11:28:14 +0200

    Finished exercise 6.14

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,22 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import anecdoteService from '../src/services/anecdote'
+import { useDispatch } from 'react-redux'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService.getAllAnecdotes().then(
+      anecdotes => dispatch(setAnecdotes(anecdotes))
+    )
+  }, [])
+
   return (
     <div>
       <h2>Anecdotes</h2>
--- part6/redux-anecdotes/store.js
+++ part6/redux-anecdotes/store.js
@@ -3,6 +3,7 @@ import anecdoteReducer from './src/reducers/anecdoteReducer'
 import filterReducer from './src/reducers/filterReducer'
 import notificationReducer from './src/reducers/notificationReducer'
 
+
 const store = configureStore({
   reducer: {
     anecdotes: anecdoteReducer,
