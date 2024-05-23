97ab50d0087bfd2fd52645c1099cd9c2139d8d69
a21dariodr <a21dariodr@iessanclemente.net>
2024-05-23 20:32:07 +0200

    Finished exercise 8.19

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react"
 import { useQuery } from "@apollo/client"
 import { ALL_BOOKS } from "../queries"
 
 const Books = (props) => {
+  const [genre, setGenre] = useState('all')
   const books = useQuery(ALL_BOOKS)
-
   if (!props.show) {
     return null
   }
@@ -14,6 +15,17 @@ const Books = (props) => {
       Loading books...
     </div>
   )
+
+  let genres = new Set([])
+  books.data.allBooks.forEach(book => {
+    genres = genres.union(new Set(book.genres))
+  })
+
+  const filteredBooks = books.data.allBooks.filter(book => {
+    return genre === 'all'
+      ? true
+      : book.genres.includes(genre)
+  })
 
   return (
     <div>
@@ -26,7 +38,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.data.allBooks.map((a) => (
+          {filteredBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -35,6 +47,11 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+
+      <div>
+        {Array.from(genres).map(genre => (<button key={genre} onClick={() => setGenre(genre)}>{genre}</button>))}
+        <button onClick={() => setGenre('all')}>all genres</button>
+      </div>
     </div>
   )
 }
